3919d1a6181dae535615bd36c3f9d72db4a36c4b
corymorin11 <54879258+corymorin11@users.noreply.github.com>
2019-10-28 20:10:11 -0600

    Initial Commit for Health Tracker

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyBHwzMghAABRAuKr5oQ6qoJdLYqQvP-uo0",
-  authDomain: "authentication2-24237.firebaseapp.com",
-  databaseURL: "https://authentication2-24237.firebaseio.com",
-  projectId: "authentication2-24237",
-  storageBucket: "",
-  messagingSenderId: "813975680001",
-  appId: "1:813975680001:web:c60653a804bd6d272a7c00"
+  apiKey: "AIzaSyAX2rSDr2TjbNkFesNN92DoTMuwIqQ_bsk",
+  authDomain: "health-tracker-90a41.firebaseapp.com",
+  databaseURL: "https://health-tracker-90a41.firebaseio.com",
+  projectId: "health-tracker-90a41",
+  storageBucket: "health-tracker-90a41.appspot.com",
+  messagingSenderId: "61788756930",
+  appId: "1:61788756930:web:346ebe79ccb7d160ae545b"
 };
 
 firebase.initializeApp(config);
